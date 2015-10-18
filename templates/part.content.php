200426645ab50d2520335884c93a257264cca162
Anthony Martel <amartel@quatral.com>
2015-10-17 22:04:14 -0400

    Fix layout and css to make it more nice

--- templates/part.content.php
+++ templates/part.content.php
@@ -1,13 +1,24 @@
 <div id="songlist"></div>
-<div class="player-container">
+<div class="player-container mpbsc noselect">
+	<div class="seek-bar-track">
+		<div class="seek-bar-progress mpbc"></div>
+		<div class="seek-bar-ball mpbc"></div>
+	</div>
 	<div class="player-interface">
-		<i class="player-button fa fa-3x fa-backward"></i>
-		<i class="player-button fa fa-3x fa-play"></i>
-		<i class="player-button fa fa-3x fa-pause"></i>
-		<i class="player-button fa fa-3x fa-forward"></i>
-		<br/>
-		<audio id="player" controls>
-			<source id="mp3-src" src="" type="audio/mpeg">
-		</audio>
+		<div class="player-left">
+			repeat
+		</div>
+		<div class="player-center">
+			<i class="player-button backward fa fa-4x fa-backward"></i>
+			<i class="player-button play-pause fa fa-4x fa-play"></i>
+			<i class="player-button forward fa fa-4x fa-forward"></i>
+			<br/>
+			<audio id="player" controls>
+				<source id="mp3-src" src="" type="audio/mpeg">
+			</audio>
+		</div>
+		<div class="player-right">
+			volume
+		</div>
 	</div>
 </div>
