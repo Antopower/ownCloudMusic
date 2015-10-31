72f4403d195cfe1f115d112ed3bc2dae923e5243
Anthony Martel <amartel@quatral.com>
2015-10-30 22:21:14 -0400

    Add render function to stop duplicating code

--- js/app.js
+++ js/app.js
@@ -4,93 +4,21 @@
         var url = OC.generateUrl('/apps/musicapi/getmusic');
         var data = {};
         $.post(url, data).success(function (response) {
-            var song;
-            var songindex = 0;
-            var songlist = '<table style="width:100%">' +
-                '<thead>' +
-                '<th>Title</th>' +
-                '<th>Artist</th>' +
-                '<th>Album</th>' +
-                '<th>Genre</th>' +
-                '<th>Year</th>' +
-                '<th>Bitrate</th>' +
-                '<th>Time</th>' +
-                '<th>Time played</th>' +
-                '</thead>'+
-                '<tbody>';
-            response.data.songs.forEach( function (song)
-            {
-                songlist += '<tr data-path="'+ song.path +'" data-index="' + songindex + '" data-id="' + song.file_id + '" class="song">';
-                songlist += '<td class="songlist-title">'+ song.title +'</td>';
-                songlist += '<td class="songlist-artist">'+ song.artist +'</td>';
-                songlist += '<td class="songlist-album">'+ song.album +'</td>';
-                songlist += '<td class="songlist-genre">'+ song.genre +'</td>';
-                songlist += '<td class="songlist-year">'+ song.year +'</td>';
-                songlist += '<td class="songlist-bitrate">'+ parseInt(song.bitrate/1000) +'kbps</td>';
-                songlist += '<td class="songlist-playtime">'+ song.play_time +'</td>';
-                songlist += '<td class="songlist-timeplayed">'+ song.time_played +'</td>';
-                songlist += '</tr>';
-                songindex = songindex + 1;
-            });
-            songlist += '</tbody></table>';
-            $('#songlist').html(songlist);
-            music.musicList = response.data.songs;
-            $('tr.song').click(function(){
-                load_track = $(this).attr('data-path');//gets me the url of the new track
-                $('.playing-song').removeClass('playing-song');
-                $(this).addClass('playing-song');
-                player.change_track($(this).attr('data-id'));// function to change the track of the loaded audio player without page refresh preferred...
-            });
+            music.render_songlist(response);
         });
     };
 
     music.add_time_played_counter = function (index) {
         var url = OC.generateUrl('/apps/musicapi/addtimeplayed');
         var data = {fileid: index};
-        $.post(url, data).success(function (response) {
-
-        });
+        $.post(url, data).success(function (response) {});
     };
 
     music.show_music_files = function () {
         var url = OC.generateUrl('/apps/musicapi/scandrivemusic');
         var data = {};
         $.post(url, data).success(function (response) {
-            var song;
-            var songlist = '<table style="width:100%">' +
-                '<thead>' +
-                '<th>Title</th>' +
-                '<th>Artist</th>' +
-                '<th>Album</th>' +
-                '<th>Genre</th>' +
-                '<th>Year</th>' +
-                '<th>Bitrate</th>' +
-                '<th>Time</th>' +
-                '<th>Time played</th>' +
-                '</thead>'+
-                '<tbody>';
-            response.songlist.forEach( function (song)
-            {
-                songlist += '<tr data-path="'+ song.path +'" class="song">';
-                songlist += '<td>'+ song.title +'</td>';
-                songlist += '<td>'+ song.artist +'</td>';
-                songlist += '<td>'+ song.album +'</td>';
-                songlist += '<td>'+ song.genre +'</td>';
-                songlist += '<td>'+ song.year +'</td>';
-                songlist += '<td>'+ parseInt(song.bitrate/1000) +'kbps</td>';
-                songlist += '<td>'+ song.play_time +'</td>';
-                songlist += '<td>'+ song.time_played +'</td>';
-                songlist += '</tr>';
-            });
-            songlist += '</tbody></table>';
-            $('#songlist').html(songlist);
-
-            $('tr.song').click(function(){
-                load_track = $(this).attr('data-path');//gets me the url of the new track
-                $('.playing-song').removeClass('playing-song');
-                $(this).addClass('playing-song');
-                player.change_track(load_track);// function to change the track of the loaded audio player without page refresh preferred...
-            });
+            music.render_songlist(response);
         });
     };
 
@@ -108,6 +36,47 @@
             if (seconds < 10) {seconds = "0"+seconds;}
         }
         return minutes+':'+seconds;
+    };
+
+    music.render_songlist = function (response) {
+        var song;
+        var songindex = 0;
+        var songlist = '<table style="width:100%">' +
+            '<thead>' +
+            '<th>Title</th>' +
+            '<th>Artist</th>' +
+            '<th>Album</th>' +
+            '<th>Genre</th>' +
+            '<th>Year</th>' +
+            '<th>Bitrate</th>' +
+            '<th>Time</th>' +
+            '<th>Time played</th>' +
+            '</thead>'+
+            '<tbody>';
+        response.data.songs.forEach( function (song)
+        {
+            songlist += '<tr data-index="' + songindex + '" data-id="' + song.file_id + '" class="song">';
+            songlist += '<td class="songlist-title">'+ song.title +'</td>';
+            songlist += '<td class="songlist-artist">'+ song.artist +'</td>';
+            songlist += '<td class="songlist-album">'+ song.album +'</td>';
+            songlist += '<td class="songlist-genre">'+ song.genre +'</td>';
+            songlist += '<td class="songlist-year">'+ song.year +'</td>';
+            songlist += '<td class="songlist-bitrate">'+ parseInt(song.bitrate/1000) +'kbps</td>';
+            songlist += '<td class="songlist-playtime">'+ song.play_time +'</td>';
+            songlist += '<td class="songlist-timeplayed">'+ song.time_played +'</td>';
+            songlist += '</tr>';
+            songindex = songindex + 1;
+        });
+        songlist += '</tbody></table>';
+        $('#songlist').html(songlist);
+        music.musicList = response.data.songs;
+        $('tr.song').click(function(){
+            load_track = $(this).attr('data-path');//gets me the url of the new track
+            $('.playing-song').removeClass('playing-song');
+            $(this).addClass('playing-song');
+            player.change_track($(this).attr('data-id'));// function to change the track of the loaded audio player without page refresh preferred...
+        });
+        return songlist;
     };
 
 }( window.music = window.music || {}, jQuery, OC ));
