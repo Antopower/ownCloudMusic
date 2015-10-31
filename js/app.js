e35330480abdd03bd4cfeb234d24f586847ac7fe
Anthony Martel <amartel@quatral.com>
2015-10-30 22:02:50 -0400

    Fix duration printing NaN after a song click and before the song start

--- js/app.js
+++ js/app.js
@@ -95,13 +95,18 @@
     };
 
     music.second_to_duration = function (second) {
-        var sec_num = parseInt(second); // don't forget the second param
-        var hours   = Math.floor(sec_num / 3600);
-        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-        var seconds = sec_num - (hours * 3600) - (minutes * 60);
+        if(second == 'NaN') {
+            minutes = '00';
+            seconds = '00';
+        } else {
+            var sec_num = parseInt(second); // don't forget the second param
+            var hours   = Math.floor(sec_num / 3600);
+            var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+            var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-        if (minutes < 10) {minutes = "0"+minutes;}
-        if (seconds < 10) {seconds = "0"+seconds;}
+            if (minutes < 10) {minutes = "0"+minutes;}
+            if (seconds < 10) {seconds = "0"+seconds;}
+        }
         return minutes+':'+seconds;
     };
 
