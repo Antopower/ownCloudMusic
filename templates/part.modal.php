7cdba16dd263be7b2bae85cdd4cf03851f773994
Anthony Martel <amartel@quatral.com>
2015-11-29 01:13:33 -0500

    Add more information to the radio modal

--- templates/part.modal.php
+++ templates/part.modal.php
@@ -1,8 +1,16 @@
 <div class="radio-modal music-app-modal">
-    <p>
-        <label for="radio-url"></label>
+    <div class="radio-modal-title">Internet Radio</div>
+    <p class="radio-modal-error"></p>
+    <div class="radio-modal-compatible-container">
+        <div class="radio-modal-compatible-title">Compatible internet radio:</div>
+        <div class="radio-modal-compatible">
+            Internet radio that use a MP3, OGG or ACC format. Radio using a Listen.pls file will NOT be compatible with this radio player.
+        </div>
+    </div>
+    <div>
+        <label for="radio-url">Url: </label>
         <input id="radio-url" name="radio-url" type="text">
-    </p>
+    </div>
     <button class="modal-cancel">Cancel</button>
     <button class="radio-go-btn">Go</button>
 </div>
