b39886885c72962f795871fbad86ac2e70a49728
Anthony Martel <amartel@quatral.com>
2015-11-30 12:36:18 -0500

    Add playlist button in menu

--- templates/part.navigation.php
+++ templates/part.navigation.php
@@ -1,3 +1,9 @@
 <ul>
 	<li><a id="music-scan" href="#">Scan your music!</a></li>
+    <div style="position: relative">
+        <li data-apps-slide-toggle="#menu-playlist-drawer"><a id="" href="#">Playlist</a></li>
+        <button id="menu-playlist-add" style="position: absolute;top: 4px;right: 4px;">+</button>
+    </div>
+	<ul id="menu-playlist-drawer" style="margin: 0 8px;background-color: #E8E8E8;">
+	</ul>
 </ul>
